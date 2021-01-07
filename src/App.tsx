2ac55e156fd5b7edcd88336b12e6a872f4771278
lucasrosate <lucas.rosate@hotmail.com>
2021-01-06 22:02:27 -0300

    Design edit 06/01/2021

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import AppStyle from './styles/AppStyle.module.css';
-import './styles/base.css';
-
 import Game from './Components/Game';
-import DigitNumbers from './Components/DigitNumbers';
 
 
 const App: React.FC = () => {
-    
+
 
 
     return (
-            <div className={AppStyle.appContainer} id="AppMain">
+        <div className={AppStyle.appContainer} id="App">
+            <header className="App-header">
                 <div className={AppStyle.title}><h1>Sudoku</h1></div>
                 <div className={AppStyle.componentsContainer} >
                     <Game />
                 </div>
-            </div>
+            </header>
+        </div>
+
+
     )
 }
 
